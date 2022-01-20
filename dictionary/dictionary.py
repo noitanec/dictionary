c3a687053e780cdf970bd9d930342792b6ab293f
Aashish Shrestha <gnoitanec@gmail.com>
2022-01-20 23:02:04 +0545

    Add support for multi-word input words

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 import sys
-import pprint
 import requests
 
 baseURL = "https://api.dictionaryapi.dev/api/v2/entries/en_US/"
 
-key = str(sys.argv[1])
+key = ' '.join(sys.argv[1:])
 URL = baseURL + key
 
 response = requests.get(url=URL)
@@ -17,8 +16,10 @@ if response:
           ['definition'])
     fields = data[0]['meanings'][0]['definitions'][0].keys()
     if 'synonyms' in fields:
-        print("\n\tSYNONYMS: ", data[0]['meanings'][0]['definitions'][0]['synonyms'], "\n")
+        print("\n\t SYNONYMS: ", data[0]['meanings']
+              [0]['definitions'][0]['synonyms'], "\n")
     if 'example' in fields:
-        print("\n\tEXAMPLE: ", data[0]['meanings'][0]['definitions'][0]['example'], "\n")
+        print("\n\t EXAMPLE: ", data[0]['meanings']
+              [0]['definitions'][0]['example'], "\n")
 else:
     print("Try another word")
